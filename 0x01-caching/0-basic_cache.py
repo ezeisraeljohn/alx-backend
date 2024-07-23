bca3c2dccdd61b940101be65c3e657d311cd7a36
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-23 19:30:05 +0100

    chore: Update get method in BasicCache to include a more descriptive docstring

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,6 +2,8 @@
 
 """ This module implements Basic Caching """
 
+from base_caching import BaseCaching
+
 
 class BasicCache(BaseCaching):
     """This class implements the logic of the base class"""
@@ -12,7 +14,7 @@ class BasicCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """Retrieves an item from the caching system dictionary"""
+        """Retrieves an item from the caching system"""
         if not key or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
