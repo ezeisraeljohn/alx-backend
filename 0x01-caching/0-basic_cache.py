0542a022b01c968b7c3fabb2d47b52b62f9d35d7
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-23 18:59:32 +0100

    chore: Add BasicCache class to implement basic caching functionality

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+""" This module implements Basic Caching """
+
+
+class BasicCache(BaseCaching):
+    """This class implements the logic of the base class"""
+
+    def put(self, key, item):
+        """This adds to the caching system"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves an item from the caching system"""
+        if not key or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
