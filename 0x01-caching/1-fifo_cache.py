8f60f3ac6bf695874524f1787ef73a2b1ee1876d
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-24 12:11:35 +0100

    Changes made to the file

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -21,7 +21,13 @@ class FIFOCache(BaseCaching):
         if len(self.cache_data.keys()) >= BaseCaching.MAX_ITEMS:
             discard_key = sorted(self.cache_data.keys())[0]
             del self.cache_data[discard_key]
-            print(f"DISCARDED {discard_key}")
             self.cache_data[key] = item
+            print(f"DISCARD: {discard_key}")
         else:
             self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves an item from the caching system"""
+        if not key or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
